7e7faf05af643d2d896945f0ab2c02752621d4f7
Bruno Vinicius <bruno.bizerra@sptech.school>
2024-09-07 01:08:09 -0300

    estruturação do Contacts em andamento

--- app/src/main/java/elder/ly/mobile/Chat1.kt
+++ app/src/main/java/elder/ly/mobile/Chat1.kt
@@ -5,10 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsEndWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,19 +37,27 @@ fun Chat1() {
     ){
         Column(
             modifier = Modifier
-                .weight(1f)
                 .background(Color.White)
                 .padding(top = 16.dp, bottom = 0.dp, start = 16.dp, end = 16.dp)
         ) {
-            Search()
-
+            SearchChat()
         }
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                items(10) {
+                    Contacts()
+                }
+            }
+
         NavBar()
     }
 }
 
 @Composable
-fun Search(){
+fun SearchChat(){
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -57,7 +73,7 @@ fun Search(){
             .border(
                 width = 1.dp,
                 color = tertiaryContainerLight,
-                shape = RoundedCornerShape(4.dp)
+                shape = RoundedCornerShape(8.dp)
             )
             .padding(8.dp)
     ) {
@@ -67,9 +83,34 @@ fun Search(){
             modifier = Modifier
                 .size(48.dp)
                 .align(alignment = Alignment.CenterStart)
-                .padding(start = 8.dp)
+                .padding(8.dp)
         )
     }
+}
+
+@Composable
+fun Contacts(){
+        Column(
+            modifier = Modifier
+                .padding(top = 16.dp, bottom = 8.dp)
+                .height(64.dp)
+        ) {
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, bottom = 8.dp, start = 24.dp, end = 24.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Foto de perfil do cuidador")
+
+                Column {
+
+                }
+
+            }
+        }
+        Divider(color = Color.LightGray, thickness = 1.dp)
 }
 
 @Preview(showBackground = true)
