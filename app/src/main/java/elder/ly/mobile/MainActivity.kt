1d0ae06b08460903af1d514d1b7ce743777e9948
LuizSuliman <luiz.suliman@sptech.school>
2024-09-08 00:28:26 -0300

    Login 3 Screen + Adress Info Screen + New Project Structure

--- app/src/main/java/elder/ly/mobile/MainActivity.kt
+++ app/src/main/java/elder/ly/mobile/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import elder.ly.mobile.ui.screens.InformacoesPessoaisEndereco
+import elder.ly.mobile.ui.screens.Login3
 import elder.ly.mobile.ui.theme.MobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,16 +16,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            MaterialTheme {
+                App()
+            }
         }
     }
 }
 
 @Composable
 fun App() {
-    Text(
-        text = "Elderly"
-    )
+    Login3()
 }
 
 @Preview(showBackground = true)
