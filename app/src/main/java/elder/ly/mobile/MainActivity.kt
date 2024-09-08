8bd96c15fa7ecf12b5c08bb4d1dd499d9b712853
katherinneassis <katherinne.silva@sptech.school>
2024-09-07 23:55:06 -0300

    @katherinneassis telas chat2, perfil e perfilusuarios

--- app/src/main/java/elder/ly/mobile/MainActivity.kt
+++ app/src/main/java/elder/ly/mobile/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.AccountCircle
@@ -19,12 +23,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import elder.ly.mobile.ui.theme.MobileTheme
+import elder.ly.mobile.ui.theme.primaryContainerLight
+import elder.ly.mobile.ui.theme.primaryLight
 import elder.ly.mobile.ui.theme.tertiaryLight
 
 class MainActivity : ComponentActivity() {
@@ -40,98 +47,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     Perfil()
-}
-
-@Composable
-fun Perfil() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(top = 30.dp, start = 16.dp, end = 16.dp)
-        ) {
-            DrawCircle()
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(80.dp),
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold,
-                fontSize = 36.sp,
-                color = Color.Black,
-                text = "Maria Antonieta"
-            )
-            MenuButton(label = "Informações Pessoais", icon = Icons.Filled.AccountCircle)
-            MenuButton(label = "Endereço", icon = Icons.Filled.Home)
-            MenuButton(label = "Profissional", icon = Icons.AutoMirrored.Filled.List)
-            MenuButton(label = "Sair", icon = Icons.Filled.ExitToApp)
-        }
-        NavBar()
-    }
-}
-
-
-@Composable
-fun MenuButton(label: String, icon: androidx.compose.ui.graphics.vector.ImageVector) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { }
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
-            modifier = Modifier.size(30.dp),
-            tint = tertiaryLight
-        )
-        Spacer(Modifier.width(16.dp))
-        Text(
-            text = label,
-            fontSize = 16.sp,
-            color = tertiaryLight,
-            modifier = Modifier.weight(1f)
-        )
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-            contentDescription = "Seta para a direita",
-            modifier = Modifier.size(30.dp),
-            tint = tertiaryLight
-        )
-    }
-    Divisor()
-}
-
-@Composable
-fun Divisor() {
-    HorizontalDivider(
-        color = tertiaryLight,
-        thickness = 1.2.dp,
-        modifier = Modifier.fillMaxWidth()
-    )
-}
-
-@Composable
-fun DrawCircle() {
-    Canvas(modifier = Modifier
-        .fillMaxWidth()
-        .height(200.dp)) {
-        drawCircle(
-            color = tertiaryLight,
-            radius = 210f,
-        )
-    }
+    PerfilOutrosUsuarios()
+    Chat2()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileTheme {
-        App()
-    }
+fun AppPreview(modifier: Modifier = Modifier) {
+    App()
 }
