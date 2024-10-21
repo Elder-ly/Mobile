9d9d871f3ebc2ef598835bc7381ca47c935a4044
Murilo Almeida <murilo.leite@sptech.school>
2024-10-20 22:23:58 -0300

    add:I added the UserService class and methods for API requests.

---
+++ app/src/main/java/elder/ly/mobile/service/CalendarService.kt
@@ -0,0 +1,37 @@
+package elder.ly.mobile.service
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import java.time.LocalDateTime
+
+interface CalendarService {
+
+    @POST("/eventos")
+    suspend fun insertEvent(@Body createEventInput : CreateEventInput) : Response<GetEventOutput>
+
+    @GET("/eventos")
+    suspend fun insertEvent(@Body createEventInput : CreateEventInput) : Response<GetEventOutput>
+}
+
+data class CreateEventInput(
+    val proposalName: String,
+    val clientEmail: String,
+    val employeeEmail: String,
+    val startDateTime: LocalDateTime,
+    val endDateTime: LocalDateTime,
+    val recurrence: String?,
+    val description: String?
+)
+
+data class GetEventOutput(
+    val id: String,
+    val proposalName: String,
+    val clientEmail: String,
+    val employeeEmail: String,
+    val startDateTime: String,
+    val endDateTime: String,
+    val recurrence: List<String>,
+    val description: String
+)
