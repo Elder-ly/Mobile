38ab8836e6332641f1a5420c94a82167818a3e22
LuizSuliman <luiz.suliman@sptech.school>
2024-11-06 20:28:23 -0300

    Merge with feature/screen-api-signupstep

--- app/src/main/java/elder/ly/mobile/MainActivity.kt
+++ app/src/main/java/elder/ly/mobile/MainActivity.kt
@@ -9,12 +9,15 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentManager.BackStackEntry
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import elder.ly.mobile.di.appModule
 import elder.ly.mobile.ui.composables.screens.addressinfo.AddressInfoScreen
 import elder.ly.mobile.ui.composables.screens.chat.ChatScreen
@@ -30,6 +33,7 @@ import elder.ly.mobile.ui.composables.screens.signupstep2.SignUpStep2Screen
 import elder.ly.mobile.ui.composables.screens.welcome.WelcomeScreen
 import elder.ly.mobile.ui.theme.MobileTheme
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -37,19 +41,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        startKoin {
-//            androidLogger()
-            androidContext(this@MainActivity) // this@MainActivity
-//            androidContext(applicationContext) // serve para a aplicação que o module vai servir pra
+        startKoin{
+            androidContext(this@MainActivity)
             modules(appModule)
         }
         setContent {
             MaterialTheme {
-                Scaffold(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .navigationBarsPadding()
-                ) { innerPadding ->
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                    .navigationBarsPadding()) { innerPadding ->
                     App(modifier = Modifier.padding(innerPadding))
                 }
             }
